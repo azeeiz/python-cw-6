1674e9c57aae29ca2e0ecae93afbf87697c5b50c
azeeiz <118297146+azeeiz@users.noreply.github.com>
2022-12-10 20:01:35 +0300

    yes

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 # write your code here
+#1
+person = {
+    'name': 'Ahmed',
+    'age': '21',
+    'hobbies': {'Biking', 'Running'}
+}
+
+#print(person['name'])
+#print(person['age'])
+
+#2
+person['age']=22
+person['country'] = 'Kuwait'
+#print(person)
+person_dictionary = person.keys()
+print(person_dictionary)
+
+
+#3
+
+person['hobbies'] = "Swimming",'Biking', 'Running', "Sleeping"
+print(person['hobbies'])
+
+def check_hobbies(person):
+    if len(person['hobbies']) > 3 :
+        print("Wow youre amazing")
+
+check_hobbies(person)
+#print(check_hobbies)
